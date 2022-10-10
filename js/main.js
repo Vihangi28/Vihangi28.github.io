4f50520d592f17196533ef99bfa9ef44b57a009a
Vihangi Fernando <110618747+Vihangi28@users.noreply.github.com>
2022-10-10 17:54:11 +0530

    Add files via upload
    Create  a personal website

---
+++ js/main.js
@@ -0,0 +1,54 @@
+(function ($) {
+    "use strict";
+    
+    // Dropdown on mouse hover
+    $(document).ready(function () {
+        function toggleNavbarMethod() {
+            if ($(window).width() > 992) {
+                $('.navbar .dropdown').on('mouseover', function () {
+                    $('.dropdown-toggle', this).trigger('click');
+                }).on('mouseout', function () {
+                    $('.dropdown-toggle', this).trigger('click').blur();
+                });
+            } else {
+                $('.navbar .dropdown').off('mouseover').off('mouseout');
+            }
+        }
+        toggleNavbarMethod();
+        $(window).resize(toggleNavbarMethod);
+    });
+    
+    // Image popup
+    function col-md-5() 
+{document.getElementById("col-md-5").src = "/wp-content/uploads/Chair_Black.jpg";}
+
+function ShowRed() 
+{document.getElementById("col-md-5").src = "/wp-content/uploads/Chair_Red.jpg";}
+    // Skills
+    $('.skills').waypoint(function () {
+        $('.progress .progress-bar').each(function () {
+            $(this).css("width", $(this).attr("aria-valuenow") + '%');
+        });
+    }, {offset: '80%'});
+    
+    //progress bar
+    var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("progressbar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
+   
+
